bef185a03c5f9bee78fa1f7dc1efa248a677db87
czarsonszef <69474346+czarsonszef@users.noreply.github.com>
2021-03-03 14:12:29 +0100

    Add files via upload

---
+++ bot_data.py
@@ -0,0 +1,36 @@
+import time
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+
+
+def get_time():
+    timevar = time.localtime()
+
+    hours = '0' + \
+        str(timevar.tm_hour) if timevar.tm_hour < 10 else str(timevar.tm_hour)
+    mins = '0' + \
+        str(timevar.tm_min) if timevar.tm_min < 10 else str(timevar.tm_min)
+    secs = '0' + \
+        str(timevar.tm_sec) if timevar.tm_sec < 10 else str(timevar.tm_sec)
+
+    return '[' + hours + ':' + mins + ':' + secs + '] '
+
+
+def log_str(arg: str):
+    if not arg:  # jesli w tweecie jest tylko zdjecie
+        return get_time()+'Odpowiedziano na tweet\'a bez tresci'
+
+    if len(arg) <= 30:
+        return get_time()+'Odpowiedziano na tweet\'a o tresci: '+arg
+
+    buff = str()
+
+    for c in arg[30:]:
+        if c == ' ':
+            break
+        buff = buff + c
+
+    return get_time()+'Odpowiedziano na tweet\'a o tresci: '+arg[:30]+buff+'...'
---
+++ main.py
@@ -0,0 +1,60 @@
+import tweepy
+import json
+from time import sleep
+from googletrans import Translator
+
+from bot_data import *
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+translator = Translator()
+
+with open('last_tweet_id.json', 'r') as f:  # setup last tweeta
+    f_obj = json.load(f)
+
+skip_first_tweet = False
+
+while True:
+    if f_obj['last_tweet_id'] == '':
+        public_tweets = api.home_timeline()
+    else:
+        public_tweets = api.home_timeline(since_id=f_obj['last_tweet_id'])
+        skip_first_tweet = True
+
+    for tweet in public_tweets[::-1]:
+        if not tweet.text:  # gdy w tweecie jest tylko zdjecie
+            continue
+
+        if tweet.text[0] == 'R' and tweet.text[1] == 'T':  # zeby nie komentowalo retweetow
+            continue
+
+        if skip_first_tweet:
+            """
+            public_tweets zawiera zawsze tweeta z zapisanym id, a nie jest pożądane komentowanie tego samego tweeta wiecej niz raz
+            """
+            skip_first_tweet = False
+            continue
+
+        if tweet.user.following == False:  # zeby nie komentowalo polecanym
+            continue
+
+        message = translator.translate(
+            tweet.text, src='pl', dest='sm').text  # samoanski
+
+        api.update_status(status='@xntentacionPL '+message,
+                          in_reply_to_status_id=tweet.id)  # wyslanie tweeta
+
+        print(log_str(tweet.text))
+
+        f_obj['last_tweet_id'] = tweet.id_str
+
+        with open('last_tweet_id.json', 'w') as f:
+            json.dump(f_obj, f)
+
+        sleep(10)
+
+    print(get_time()+'Czekam...')
+    sleep(180)
