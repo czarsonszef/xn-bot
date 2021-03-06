193d67e56e6c9b2bbfe914b4795fc00a3cd6ee87
czarsonszef <czarson2115@wp.pl>
2021-03-06 04:34:42 +0100

    .

--- bot_data.py
+++ bot_data.py
@@ -1,4 +1,6 @@
 import time
+import json
+from tweepy import RateLimitError
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
@@ -6,9 +8,8 @@ ACCESS_TOKEN = ''
 ACCESS_TOKEN_SECRET = ''
 
 
-def get_time():
+def get_time() -> dict:
     timevar = time.localtime()
-
     hours = '0' + \
         str(timevar.tm_hour) if timevar.tm_hour < 10 else str(timevar.tm_hour)
     mins = '0' + \
@@ -16,23 +17,83 @@ def get_time():
     secs = '0' + \
         str(timevar.tm_sec) if timevar.tm_sec < 10 else str(timevar.tm_sec)
 
-    return '[' + hours + ':' + mins + ':' + secs + '] '
+    return {
+        'log_string': '[' + hours + ':' + mins + ':' + secs + '] ',
+        'no_brackets_time': hours + ':' + mins + ':' + secs,
+        'month_int': timevar.tm_mon,
+        'day_int': timevar.tm_mday
+    }
 
 
-def log_str(arg: str):
+def append_json(file_name: str, mapa):
+    try:
+        with open(file_name, 'r') as f:
+            f_obj = json.load(f)
+    except FileNotFoundError:
+        print(
+            f'{get_time()["log_string"]}Nie znaleziono pliku o nazwie {file_name}')
+        return
+
+    f_obj = f_obj + mapa
+    with open(file_name, 'w') as f:
+        json.dump(f_obj, f)
+
+
+# def print_log(*args):
+#    print(get_time()['log_string'], end="")
+#    print(*args)
+# TODO
+
+def log(arg: str):
     if len(arg) <= 30:
-        return get_time()+'Odpowiedziano na tweet\'a o tresci: '+arg
+        log = 'Odpowiedziano na tweet\'a o tresci: '+arg
+    else:
+        buff = str()
+        for c in arg[30:]:
+            if c == ' ':
+                break
+            buff = buff + c
+        log = 'Odpowiedziano na tweet\'a o tresci: ' + \
+            arg[:30]+buff+'...'
+    print(get_time()['log_string']+log)
 
-    buff = str()
+    log_map = [{
+        'month': get_time()['month_int'],
+        'day': get_time()['day_int'],
+        'time': get_time()['no_brackets_time'],
+        'log': log
+    }]
+    append_json('logi.json', log_map)
 
-    for c in arg[30:]:
-        if c == ' ':
-            break
-        buff = buff + c
 
-    log = get_time()+'Odpowiedziano na tweet\'a o tresci: '+arg[:30]+buff+'...'
+def handle_error(error: BaseException):
+    if error == RateLimitError:
+        error_msg = f'{get_time()["log_string"]}Rate limited'
+        print(error_msg)
 
-    with open("logi.txt", "w") as f:
-        f.write(log+'\n')
+        log_map = [{
+            'month': get_time()['month_int'],
+            'day': get_time()['day_int'],
+            'time': get_time()['no_brackets_time'],
+            'log': "ERROR: Rate limited"
+        }]
+        append_json('logi.json', log_map)
 
-    return log
+        time.sleep(900)
+        return
+
+    error_msg = f'{get_time()["log_string"]}Błąd'
+    print(error_msg)
+
+    log_map = log_map = [{
+        'month': get_time()['month_int'],
+        'day': get_time()['day_int'],
+        'time': get_time()['no_brackets_time'],
+        'log': "ERROR: "+str(error)
+    }]
+    append_json('logi.json', log_map)
+
+
+if __name__ == '__main__':
+    # TODO: GRAPH
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3.9
 import tweepy
 import json
 from time import sleep
@@ -6,9 +5,9 @@ from googletrans import Translator
 
 from bot_data import *
 
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
 api = tweepy.API(auth)
 
 translator = Translator()
@@ -17,35 +16,36 @@ with open('last_tweet_id.json', 'r') as f:  # setup last tweeta
     f_obj = json.load(f)
 
 while True:
-    if f_obj['last_tweet_id'] == '':
-        public_tweets = api.home_timeline()
-    else:
-        public_tweets = api.home_timeline(since_id=f_obj['last_tweet_id'])
+    try:
+        if f_obj['last_tweet_id'] == '':
+            public_tweets = api.home_timeline()
+        else:
+            public_tweets = api.home_timeline(since_id=f_obj['last_tweet_id'])
 
-    for tweet in public_tweets[::-1]:
-        if not tweet.text:  # gdy w tweecie jest tylko zdjecie
-            continue
-            
-        if not tweet.user.following:  # zeby nie komentowalo polecanym
-            continue
+        for tweet in public_tweets[::-1]:
+            # czasem sie pojawiaja w home_timeline() jakies stare tweety
+            if get_time()['day_int']-tweet.created_at.day > 2 or (get_time()['day_int']-tweet.created_at.day <= -28 and get_time()['day_int']-tweet.created_at.day >= -30):
+                continue
+            if not tweet.text:  # gdy w tweecie jest tylko zdjecie
+                continue
+            if tweet.text[:2] == 'RT':  # zeby nie komentowalo retweetow
+                continue
+            if not tweet.user.following:  # zeby nie komentowalo polecanym
+                continue
 
-        if tweet.text[:2] == 'RT':  # zeby nie komentowalo retweetow
-            continue
+            message = translator.translate(
+                tweet.text, src='pl', dest='sm').text  # samoanski
+            api.update_status(status=f'@xntentacionPL {message}',
+                              in_reply_to_status_id=tweet.id)  # wyslanie tweeta
+            api.create_favorite(tweet.id)
 
-        message = translator.translate(
-            tweet.text, src='pl', dest='sm').text  # samoanski
+            log(tweet.text)
 
-        api.update_status(status='@xntentacionPL '+message,
-                          in_reply_to_status_id=tweet.id)  # wyslanie tweeta
+            f_obj['last_tweet_id'] = tweet.id_str
+            append_json('last_tweet_id.json', f_obj)
+            sleep(10)
+    except BaseException as e:
+        handle_error(e)
 
-        print(log_str(tweet.text))
-
-        f_obj['last_tweet_id'] = tweet.id_str
-
-        with open('last_tweet_id.json', 'w') as f:
-            json.dump(f_obj, f)
-
-        sleep(10)
-
-    print(get_time()+'Czekam...')
+    print(f'{get_time()["log_string"]}Czekam...')
     sleep(180)
